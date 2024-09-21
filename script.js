d43fdc6660843aa4e80edd3613219f017240f792
recyclebin12331 <guelbertgabunada@gmail.com>
2024-09-21 20:14:32 +0800

    improved <3

---
+++ script.js
@@ -0,0 +1,59 @@
+const video1 = document.querySelector('.video1');
+video1.addEventListener('mouseenter', () => {
+    video1.play();
+});
+
+video1.addEventListener('mouseleave', () => {
+    video1.pause();
+    video1.currentTime = 0;
+});
+
+const video2 = document.querySelector('.video2');
+video2.addEventListener('mouseenter', () => {
+    video2.play();
+});
+
+video2.addEventListener('mouseleave', () => {
+    video2.pause();
+    video2.currentTime = 0;
+});
+
+const video3 = document.querySelector('.video3');
+video3.addEventListener('mouseenter', () => {
+    video3.play();
+});
+
+video3.addEventListener('mouseleave', () => {
+    video3.pause();
+    video3.currentTime = 0;
+});
+
+const video4 = document.querySelector('.video4');
+video4.addEventListener('mouseenter', () => {
+    video4.play();
+});
+
+video4.addEventListener('mouseleave', () => {
+    video4.pause();
+    video4.currentTime = 0;
+});
+
+const video5 = document.querySelector('.video5');
+video5.addEventListener('mouseenter', () => {
+    video5.play();
+});
+
+video5.addEventListener('mouseleave', () => {
+    video5.pause();
+    video5.currentTime = 0;
+});
+
+const video6 = document.querySelector('.video6');
+video6.addEventListener('mouseenter', () => {
+    video6.play();
+});
+
+video6.addEventListener('mouseleave', () => {
+    video6.pause();
+    video6.currentTime = 0;
+});
